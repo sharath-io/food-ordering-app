976e7301c564fac87ae589612d320594f6a79280
Sharath <sharath.esr@gmail.com>
2023-05-10 19:18:11 +0530

    added home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,10 @@
+import {NavLink} from 'react-router-dom';
 export function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Welcome to neoG Food Ordering App</h1>
+            <NavLink to="/menu"><button>Menu</button></NavLink>
+
         </div>
     )
 }
